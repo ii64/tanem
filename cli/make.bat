89fb3c61f0870a82a06d770f750a2ab0cb8d1441
ii64 <richiisei@gmail.com>
2020-12-17 14:47:50 +0700

    upd ci,.,.d././

--- cli/make.bat
+++ cli/make.bat
@@ -1,2 +1,2 @@
 @ECHO off
-go build -i -v -ldflags "-X github.com/ii64/tanem/cmd.GIT_COMMIT=local" tanem.go
+go build -i -v -ldflags "-X github.com/ii64/tanem/cmd.GIT_COMMIT=local -lpthread" tanem.go
